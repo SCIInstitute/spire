bf562f2cfd281c73ca673fa4459a906a344a7f0b
James Hughes <jhughes@sci.utah.edu>
2012-12-04 13:03:34 -0700

    Reorganized project. No longer include the src folder.

--- scripts/bldView.sh
+++ scripts/bldView.sh
@@ -5,7 +5,7 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 mkdir -p ../bin
 pushd ../bin > /dev/null
-  cmake -D CMAKE_BUILD_TYPE="Debug" ../src/view
+  cmake -D CMAKE_BUILD_TYPE="Debug" ../tools/view
   make -j4
 popd > /dev/null
 
---
+++ tools/view/GLContext.h
@@ -0,0 +1,59 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2012 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+/// \author James Hughes
+/// \date   December 2012
+
+#ifndef TOOLS_VIEW_GLCONTEXT_H
+#define TOOLS_VIEW_GLCONTEXT_H
+
+#include <QtOpenGL/QGLWidget>
+
+// Include spire interface. There has got to be a better way of indicating that
+// we are including spire.
+#include "../../Interface.h"
+
+/// Context that will be sent to spire.
+class GLContext : public Spire::Context
+{
+public:
+  GLContext(QGLWidget* glWidget);
+  virtual ~GLContext();
+
+  /// Mandatory override from Context.
+  virtual void makeCurrent();
+  
+  /// Mandatory override from Context.
+  virtual void swapBuffers();
+
+private:
+
+  QGLWidget* mGLWidget;
+};
+
+#endif 
