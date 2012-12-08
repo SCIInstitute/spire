3508224531b9b5bedd1ae3eded7f581bf17d11ad
James Hughes <jhughes@sci.utah.edu>
2012-12-07 18:17:36 -0700

    Removed scripts directory.
    It may return in the future with scripts that are useful across all
    platforms.

--- scripts/mclangcomp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-find ../bin -name ".clang_complete" -exec cat {} >> .clang_complete_temp \;
-
-# Remove duplicates
-sort .clang_complete_temp | uniq > .clang_complete
-mv .clang_complete ../
-rm .clang_complete_temp
-
