3a9a3f21015f53cc555316876ff44a88f047cab9
James Hughes <jhughes@sci.utah.edu>
2013-03-22 20:50:36 -0600

    Adding one dllexport command so windows will generate .lib...

--- Spire/Core/PipeInterface.h
+++ Spire/Core/PipeInterface.h
@@ -34,6 +34,14 @@
 
 #include "../Interface.h"
 
+#if defined(_WIN32)
+// One dllexport must be present in a shared library in order for a .lib
+// file to be generated...
+#define WIN_DLL_EXPORT __declspec(dllexport)
+#else
+#define WIN_DLL_EXPORT
+#endif
+
 namespace Spire
 {
 
@@ -44,7 +52,7 @@ class Hub;
 ///       should become more clear.
 /// It will be mandatory to pointer cast this pipe to its derived
 /// type in order to access the majority of the interface.
-class PipeInterface
+class WIN_DLL_EXPORT PipeInterface
 {
 public:
   // Does NOT store a reference to the interface...
