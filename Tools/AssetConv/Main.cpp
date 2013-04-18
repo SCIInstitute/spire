ef872d0250865b632ee465d9dff5e907db4ff76c
James Hughes <jhughes@sci.utah.edu>
2013-04-18 10:43:42 -0600

    Fixed command line parsing in importer.

--- Tools/AssetConv/Main.cpp
+++ Tools/AssetConv/Main.cpp
@@ -81,6 +81,8 @@ int main(int argc, char* argv[])
 
     //cmd.xorAdd(input, directory);
     cmd.add(input);
+    cmd.add(output);
+    cmd.parse(argc, argv);
 
     inFile = input.getValue();
     outFile = output.getValue();
@@ -91,9 +93,12 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
+  std::cout << "Input file: " << inFile << std::endl;
+  std::cout << "Output file: " << outFile << std::endl;
+
   // Initialize assimp.
   createAssimpLogger();
-  //Assimp::DefaultLogger::get()->info("Assimp initialized.");
+  std::cout << "Assimp initialized." << std::endl;
 
   // Determine IL Version 
   if (ilGetInteger(IL_VERSION_NUM) < IL_VERSION)
@@ -101,7 +106,7 @@ int main(int argc, char* argv[])
     ILint test = ilGetInteger(IL_VERSION_NUM);
     std::string errMsg = "Wrong DevIL version.";
     char* cErrMsg = (char*)errMsg.c_str();
-    //Assimp::DefaultLogger::get()->info(cErrMsg);
+    std::cout << "IL failure: " << cErrMsg << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -111,7 +116,7 @@ int main(int argc, char* argv[])
   Assimp::Importer importer;
 
   scene = importer.ReadFile(inFile,
-    //aiProcess_MakeLeftHanded |
+    aiProcess_MakeLeftHanded |
     aiProcess_FlipWindingOrder |
     aiProcess_CalcTangentSpace |
     aiProcess_LimitBoneWeights |
@@ -128,7 +133,7 @@ int main(int argc, char* argv[])
 
   if (!scene)
   {
-    //Assimp::DefaultLogger::get()->info(importer.GetErrorString());
+    std::cout << "Error: " << importer.GetErrorString() << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -172,20 +177,50 @@ int main(int argc, char* argv[])
     output.write(reinterpret_cast<const char*>(&mesh->mNumFaces), sizeof(uint32_t));
     for (size_t j = 0; j < mesh->mNumFaces; j++)
     {
-      assert(mesh->mFaces[j].mNumIndices == 3);
+      if (mesh->mFaces[j].mNumIndices == 3)
+      {
+        // Handle triangles.
 
-      // The following is a precaution until we can start using 32 bit data.
-      // But we still write the indices out using 32 bit data.
-      assert(mesh->mFaces[j].mIndices[0] < 65536);
-      assert(mesh->mFaces[j].mIndices[1] < 65536);
-      assert(mesh->mFaces[j].mIndices[2] < 65536);
+        // The following is a precaution until we can start using 32 bit data.
+        // But we still write the indices out using 32 bit data.
+        assert(mesh->mFaces[j].mIndices[0] < 65536);
+        assert(mesh->mFaces[j].mIndices[1] < 65536);
+        assert(mesh->mFaces[j].mIndices[2] < 65536);
 
-      uint16_t index0 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[0]);
-      uint16_t index1 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[1]);
-      uint16_t index2 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[2]);
-      output.write(reinterpret_cast<const char*>(&index0), sizeof(uint16_t));
-      output.write(reinterpret_cast<const char*>(&index1), sizeof(uint16_t));
-      output.write(reinterpret_cast<const char*>(&index2), sizeof(uint16_t));
+        uint16_t index0 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[0]);
+        uint16_t index1 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[1]);
+        uint16_t index2 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[2]);
+        output.write(reinterpret_cast<const char*>(&index0), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index1), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index2), sizeof(uint16_t));
+      }
+      else if (mesh->mFaces[j].mNumIndices == 4)
+      {
+        // Handle quads. Need to convert to triangles. Ensure to swap winding
+        // order of the quads.
+        
+        // The following is a precaution until we can start using 32 bit data.
+        // But we still write the indices out using 32 bit data.
+        assert(mesh->mFaces[j].mIndices[0] < 65536);
+        assert(mesh->mFaces[j].mIndices[1] < 65536);
+        assert(mesh->mFaces[j].mIndices[2] < 65536);
+        assert(mesh->mFaces[j].mIndices[3] < 65536);
+
+        uint16_t index0 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[0]);
+        uint16_t index1 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[1]);
+        uint16_t index2 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[2]);
+        uint16_t index3 = static_cast<uint16_t>(mesh->mFaces[j].mIndices[3]);
+
+        // First triangle.
+        output.write(reinterpret_cast<const char*>(&index0), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index1), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index2), sizeof(uint16_t));
+
+        // Second triangle (opposite winding order).
+        output.write(reinterpret_cast<const char*>(&index3), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index2), sizeof(uint16_t));
+        output.write(reinterpret_cast<const char*>(&index1), sizeof(uint16_t));
+      }
     }
   }
 
