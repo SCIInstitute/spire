f9d0e0eebdd1f58f09d8a3ca25bd05f8b29fc55d
James Hughes <jhughes@sci.utah.edu>
2013-07-02 20:20:42 -0600

    Initial implementation of per-state global uniforms.

---
+++ Spire/Core/PassUniformStateMan.cpp
@@ -0,0 +1,186 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2013 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+/// \author James Hughes
+/// \date   February 2013
+
+#include "Common.h"
+#include "PassUniformStateMan.h"
+#include "ShaderUniformMan.h"
+#include "Hub.h"
+#include "Exceptions.h"
+
+namespace Spire {
+
+//------------------------------------------------------------------------------
+PassUniformStateMan::PassUniformStateMan(Hub& hub) :
+    mHub(hub)
+{
+}
+
+//------------------------------------------------------------------------------
+PassUniformStateMan::~PassUniformStateMan()
+{
+}
+
+//------------------------------------------------------------------------------
+PassUniformStateMan::PassUniforms* PassUniformStateMan::getPass(const std::string& pass)
+{
+  // Not very efficient -- but still more efficient than other methods when the
+  // number of passes is low.
+  for (int i = 0; i < mPasses.size(); i++)
+  {
+    if (mPasses[i].passName == pass)
+    {
+      return &mPasses[i];
+    }
+  }
+
+  return nullptr;
+}
+
+//------------------------------------------------------------------------------
+const PassUniformStateMan::PassUniforms* PassUniformStateMan::getPass(const std::string& pass) const
+{
+  // Not very efficient -- but still more efficient than other methods when the
+  // number of passes is low.
+  for (int i = 0; i < mPasses.size(); i++)
+  {
+    if (mPasses[i].passName == pass)
+    {
+      return &mPasses[i];
+    }
+  }
+
+  return nullptr;
+}
+
+//------------------------------------------------------------------------------
+PassUniformStateMan::PassUniforms& PassUniformStateMan::getOrCreatePass(const std::string& pass)
+{
+  PassUniforms* passStruct = getPass(pass);
+  if (passStruct == nullptr)
+  {
+    // Add a new pass, and return that.
+    PassUniforms passUniforms;
+    passUniforms.passName = pass;
+    mPasses.push_back(passUniforms);
+    return mPasses.back();
+  }
+  else
+  {
+    return *passStruct;
+  }
+}
+
+//------------------------------------------------------------------------------
+bool PassUniformStateMan::tryApplyUniform(const std::string& pass, 
+                                       const std::string& name, int location)
+{
+  // We use mState.at instead of the [] operator because at throws an
+  // exception if the key is not found in the container.
+  /// \xxx  Possibly detect if the name exists, and if it does not then throw
+  ///       a different exception (instead of letting the map throw std::range_error).
+  PassUniforms* passStruct = getPass(pass);
+  if (passStruct != nullptr)
+  {
+    std::shared_ptr<AbstractUniformStateItem> ptr = passStruct->uniforms[name];
+    if (ptr != nullptr)
+    {
+      ptr->applyUniform(location);
+      return true;
+    }
+  }
+  return false;
+}
+
+//------------------------------------------------------------------------------
+void PassUniformStateMan::updatePassUniform(const std::string& pass, const std::string& name, 
+                                            std::shared_ptr<AbstractUniformStateItem> item)
+{
+  std::shared_ptr<const UniformState> uniform = mHub.getShaderUniformManager().findUniformWithName(name);
+  if (uniform == nullptr)
+  {
+    // Default to adding the uniform to the uniform manager.
+    mHub.getShaderUniformManager().addUniform(name, ShaderUniformMan::uniformTypeToGL(item->getGLType()));
+    uniform = mHub.getShaderUniformManager().getUniformWithName(name); // std::out_of_range
+  }
+
+  // Double check that the uniform we are receiving matches types.
+  GLenum incomingType = ShaderUniformMan::uniformTypeToGL(item->getGLType());
+  if (incomingType != uniform->type)
+    throw ShaderUniformTypeError("Incoming type does not match type stored in uniform!");
+
+  // Retrieve the appropriate pass and add/update our uniform.
+  PassUniforms& passStruct = getOrCreatePass(pass);
+  passStruct.uniforms[name] = item;
+}
+
+//------------------------------------------------------------------------------
+std::shared_ptr<const AbstractUniformStateItem>
+PassUniformStateMan::getPassUninform(const std::string& pass, const std::string& name) const
+{
+  const PassUniforms* passStruct = getPass(pass);
+  if (passStruct != nullptr)
+  {
+    auto it = passStruct->uniforms.find(name);
+    if (it != passStruct->uniforms.end())
+    {
+      return it->second;
+    }
+    else
+    {
+      return std::shared_ptr<AbstractUniformStateItem>(); 
+    }
+  }
+  else
+  {
+    return std::shared_ptr<AbstractUniformStateItem>();
+  }
+}
+
+//------------------------------------------------------------------------------
+std::string PassUniformStateMan::uniformAsString(const std::string& pass, const std::string& name) const
+{
+  const PassUniforms* passStruct = getPass(pass);
+  if (passStruct != nullptr)
+  {
+    auto it = passStruct->uniforms.find(name);
+    if (it != passStruct->uniforms.end())
+      return it->second->asString();
+    else
+      return "";
+  }
+  else
+  {
+    return "";
+  }
+}
+
+
+} // end of namespace Spire
---
+++ Spire/Core/PassUniformStateMan.h
@@ -0,0 +1,116 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2013 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+/// \author James Hughes
+/// \date   February 2013
+
+#ifndef SPIRE_CORE_PASSUNIFORMSTATEMAN_H
+#define SPIRE_CORE_PASSUNIFORMSTATEMAN_H
+
+#include <map>
+#include <unordered_map>
+
+#include "ShaderUniformStateManTemplates.h"
+
+namespace Spire {
+
+class Hub;
+
+/// Unform state management. The currently available uniform state can be
+/// set and queried from this interface.
+class PassUniformStateMan
+{
+public:
+  PassUniformStateMan(Hub& hub);
+  virtual ~PassUniformStateMan();
+  
+  /// Adds a uniform to the global state.
+  /// Throws std::out_of_range if a uniform of corresponding name is not found
+  /// in UniformManager.
+  /// \param  name    Name, as used in the shader, of the uniform.
+  /// \param  data    Data to be associated with the uniform.
+  ///                 If you supply an invalid type for data, you will encounter
+  ///                 a compile-time error telling you as such. See
+  ///                 ShaderUniformStateManTemplates.h for a list of datatypes
+  ///                 accepted by this function (look at the specializations).
+  template <typename T>
+  void addUniform(const std::string& pass, const std::string& name, T data)
+  {
+    updatePassUniform(pass, name, 
+                      std::shared_ptr<AbstractUniformStateItem>(UniformStateItem<T>(data)));
+  }
+
+  /// Updates the global uniform state with the given state item.
+  /// If the item does not already exist, it will be created.
+  /// Throws std::out_of_range if a uniform of corresponding name is not found
+  /// in UniformManager.
+  void updatePassUniform(const std::string& pass, const std::string& name, 
+                         std::shared_ptr<AbstractUniformStateItem> item);
+
+  /// Attempts to apply the specified uniform to the current shader state.
+  /// Returns false 
+  bool tryApplyUniform(const std::string& pass, const std::string& name, int location);
+
+  /// Retrieves the texture representation of the uniform with 'name'.
+  /// This *really* should return std::optional.
+  std::string uniformAsString(const std::string& pass, const std::string& name) const;
+
+  /// Retrieves the abstract item representing a global uniform.
+  /// An exception is thrown if the global uniform of specified name does not
+  /// exist.
+  std::shared_ptr<const AbstractUniformStateItem> getPassUninform(
+      const std::string& pass, const std::string& name) const;
+
+private:
+
+  /// Structures containing all of the uniforms in a pass.
+  struct PassUniforms
+  {
+    std::string passName;
+    std::unordered_map<std::string, std::shared_ptr<AbstractUniformStateItem>> uniforms;
+  };
+
+  // Retrieves a pre-existing pass. If there exists no pass then create it.
+  PassUniforms& getOrCreatePass(const std::string& pass);
+
+  // Retrieval of pass uniforms.
+  // Using pointers to mimic the 'optional' type. std::optional will be in
+  // C++14.
+  PassUniforms* getPass(const std::string& pass);
+  const PassUniforms* getPass(const std::string& pass) const;
+
+  // We're not likely to have very many pass uniforms. So this is just a vector
+  // for now.
+  std::vector<PassUniforms>   mPasses;
+  Hub&                        mHub;
+};
+
+
+} // namespace Spire 
+
+#endif 
