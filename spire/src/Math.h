ac99821c8824bfa652aef2d01380b61f8b2a9727
James Hughes <jhughes@sci.utah.edu>
2013-10-25 10:54:24 -0600

    Get rid of superfluous cpm include.

--- spire/src/Math.h
+++ spire/src/Math.h
@@ -67,7 +67,6 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_inverse.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "cpm/cpm.h"
 
 namespace CPM_SPIRE_NS {
 namespace spire {
